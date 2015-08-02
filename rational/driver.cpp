e57962ab32520c7ffbca30fb3e86ae19442886aa
Mal Ganis <nissan18@gmail.com>
2015-08-01 20:37:55 -0700

    Added tests for arithmetic operators

--- rational/driver.cpp
+++ rational/driver.cpp
@@ -4,8 +4,14 @@
 using namespace std;
 
 int main() {
-  Rational r(3,5);
-  cout << "Malganis\n";
-  cout << r << endl;
+  Rational r(3,5), q(1,2);
+  cout << "Hello rational\n";
+  cout << r << ", " << q << endl;
+
+  // cout << r << " + " <<  q << " = " << r + q << endl;
+  // cout << r << " - " <<  q << " = " << r - q << endl;
+  // cout << r << " * " <<  q << " = " << r * q << endl;
+  // cout << r << " / " <<  q << " = " << r / q << endl;
+
   return 0;
 }
