e6abea504646a2b7074cc88cc9ed009edc6df28c
Mal Ganis <nissan18@gmail.com>
2015-08-10 19:36:18 -0700

    Basic if-else program

---
+++ noif/driver.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+char getLetterGrade(int grade) {
+  if (grade >= 90)
+    return 'A';
+  else if (grade >= 80)
+    return 'B';
+  else if (grade >= 70)
+    return 'C';
+  else if (grade >= 60)
+    return 'D';
+  else
+    return 'F';
+
+}
+
+int main() {
+  int grade;
+  cout << "Enter grade: ";
+  cin >> grade;
+  char letter = getLetterGrade(grade);
+  cout << "The letter grade is: " << letter << endl;
+
+  return 0;
+}
